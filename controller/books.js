6c127e436fc385c6ab3eaa121c73dea4ed5121f0
Hanif Samad <hanif.samad.sg@gmail.com>
2017-07-13 19:16:28 +0800

    fixed createBook controller and routes

--- controller/books.js
+++ controller/books.js
@@ -87,9 +87,6 @@ exports.createBook = (req, res, next) => {
 
     const book = new Book();
 
-    cloudinary.uploader.upload(req.file.path, (result) => {
-      book.cover = result.secure_url || "Unknown";
-    });
     book.title = req.body.title || "Unknown";
     book.author = req.body.author || "Unknown";
     book.genre = req.body.genre || "Unknown";
@@ -97,39 +94,71 @@ exports.createBook = (req, res, next) => {
     book.review = req.body.review || "Unknown";
     book.reserved = false;
     book.reservedBy = null;
-    book.save((err, book) => {
+    cloudinary.uploader.upload(req.file.path, (result) => {
+      book.cover = result.secure_url || "Unknown";
+      book.save((err, book) => {
 
-      /* Update user model with saved book */
-      const user = req.user;
-      console.log("saved book, now finding book owner");
-      console.log(user);
+        /* Update user model with saved book */
+        const user = req.user;
+        console.log("saved book, now finding book owner");
+        console.log(user);
 
-      User.findById(user._id, (err, foundUser) => {
-        if (err) return res.status(400).send('Bad Request');
-
-        if(!foundUser){
-          return res.status(404).send('User not Found');
-        }
-
-        foundUser.booksOwned.push(book._id);
-        foundUser.save( (err, savedUser) => {
+        User.findById(user._id, (err, foundUser) => {
           if (err) return res.status(400).send('Bad Request');
-          console.log("pushed book._id into User booksOwned");
-          console.log(savedUser);
-        });
 
+          if(!foundUser){
+            return res.status(404).send('User not Found');
+          }
+          console.log("Found user!")
+          foundUser.booksOwned.push(book._id);
+          foundUser.save( (err, savedUser) => {
+            if (err) return res.status(400).send('Bad Request');
+            console.log("pushed book._id into User booksOwned");
+            console.log(savedUser);
+          });
+        });
+        res.json(book);
       });
-         res.json(book);
-    });
+    })
 }
 
+/*
+* Release book helper function
+*/
+
+const releaseBook = (foundBook) => {
+  /* Find borrower and release book from his borrowed list */
+  const borrowerId = foundBook.reservedBy
+
+  User.findById(borrowerId, (err, foundBorrower) => {
+    if (err) return res.status(400).send('Bad Request');
+
+    if(!foundBorrower){
+      return res.status(404).send('User not Found');
+    }
+    console.log("found borrower");
+
+    const booksBorrowed = foundBorrower.booksBorrowed;
+    booksBorrowed.splice(booksBorrowed.indexOf(borrowerId),1);
+
+    foundBorrower.save( (err, savedBorrower) => {
+      if (err) return res.status(400).send('Bad Request');
+      console.log("removed book._id from User booksBorrowed");
+      console.log(savedBorrower);
+    });
+  });
+
+  foundBook.reservedBy = null;
+  foundBook.reserved = false;
+  return foundBook;
+}
 
 /*
-*  Owner updates book
+*  Owner updates book with pic
 */
-exports.updateBook = (req, res, next) => {
+exports.updateBookWithPic = (req, res, next) => {
 
-  console.log("Got request updateBook");
+  console.log("Got request updateBookWithPic");
 
   const id = req.params.id;
   const book = req.body;
@@ -142,45 +171,57 @@ exports.updateBook = (req, res, next) => {
      }
      console.log("Found book, now updating");
 
-    // cloudinary.uploader.upload(req.file.path, (result) => {
-    //   foundBook.cover = result.secure_url;
-    // });
-    //  foundBook.title = book.title;
-    //  foundBook.author = book.author;
-    //  foundBook.genre = book.genre;
-    //  foundBook.review = book.review;
+     foundBook.title = book.title;
+     foundBook.author = book.author;
+     foundBook.genre = book.genre;
+     foundBook.review = book.review;
 
      if(book.release == 'Yes') {
-
-       /* Find borrower and release book from his borrowed list */
-       const borrowerId = foundBook.reservedBy
-
-       User.findById(borrowerId, (err, foundBorrower) => {
-         if (err) return res.status(400).send('Bad Request');
-
-         if(!foundBorrower){
-           return res.status(404).send('User not Found');
-         }
-         console.log("found borrower");
-
-         const booksBorrowed = foundBorrower.booksBorrowed;
-         booksBorrowed.splice(booksBorrowed.indexOf(borrowerId),1);
-
-         foundBorrower.save( (err, savedBorrower) => {
-           if (err) return res.status(400).send('Bad Request');
-           console.log("removed book._id from User booksBorrowed");
-           console.log(savedBorrower);
-         });
-       });
-
-       foundBook.reservedBy = null;
-       foundBook.reserved = false;
+       foundBook = releaseBook(foundBook);
      }
 
-     console.log(foundBook);
+     cloudinary.uploader.upload(req.file.path, (result) => {
+       foundBook.cover = result.secure_url;
+       console.log(foundBook);
+       foundBook.save((err, updatedBook)=> {
+         if (err) return res.status(400).send('Bad Request');
+         console.log("updated book with pic");
+         res.json(updatedBook);
+       });
+     },{public_id: req.body.picReviewPublicId})
+  });
+}
+
+/*
+*  Owner updates book without pic
+*/
+exports.updateBookNoPic = (req, res, next) => {
+
+  console.log("Got request updateBookNoPic");
+
+  const id = req.params.id;
+  const book = req.body;
+
+  Book.findById(id, (err, foundBook) => {
+     if (err) return res.status(400).send('Bad Request');
+
+     if(!foundBook){
+       return res.status(404).send('Not Found');
+     }
+     console.log("Found book, now updating");
+
+     foundBook.title = book.title;
+     foundBook.author = book.author;
+     foundBook.genre = book.genre;
+     foundBook.review = book.review;
+
+     if(book.release == 'Yes') {
+       foundBook = releaseBook(foundBook);
+     }
+
      foundBook.save((err, updatedBook)=> {
        if (err) return res.status(400).send('Bad Request');
-       console.log("updated book");
+       console.log("updated book no pic");
        res.json(updatedBook);
      });
   });
@@ -225,13 +266,7 @@ exports.reserveBook = (req, res, next) => {
         });
 
       });
-
       res.json(book);
-      // book.populate('owner')
-      //     .populate('reservedBy')
-      //     .exec((err,populatedBook) => {
-      //       res.json(populatedBook);
-      //     });
     });
   });
 }
